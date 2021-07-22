752de4ab8d854832dde339416f5c0c0c1d5d77e2
Saloni <subedisaloni2002@gmail.com>
2021-07-22 16:42:19 +0530

    Add procfile, update ports

--- app.js
+++ app.js
@@ -401,6 +401,11 @@ app.get("/about", (req, res) => {
   res.render("about", { authenticated: req.isAuthenticated() });
 });
 
-app.listen(3000, function () {
-  console.log("Server is running on port 3000");
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+}
+
+app.listen(port, function () {
+  console.log("Server has started successfully");
 });
